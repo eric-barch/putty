0285d6ac5a5cf30c7e5c7a24762061a598aa1532
Eric Barch <eric.michael.barch@gmail.com>
2024-03-30 18:29:55 -0400

    Connect Prisma to server, migrate initial schema

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import net from "net";
 
 const deweyApi = express();
 const port = 3000;
+
+deweyApi.get("/test-db", (req, res) => {
+  const client = new net.Socket();
+
+  client.connect(5432, "db", () => {
+    res.send("Connected to server!");
+    client.end();
+  });
+
+  client.on("error", (err) => {
+    res.status(500).send(`Error connecting to db: ${err.message}`);
+  });
+});
 
 deweyApi.get("/", (req, res) => {
   res.send("Hello, World!");
