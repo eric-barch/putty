0448f277892e24e0dbe813b8c6aedb154adc95af
Eric Barch <eric.michael.barch@gmail.com>
2024-03-30 10:25:20 -0400

    Express hello world working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+
+const deweyApi = express();
+const port = 3000;
+
+deweyApi.get("/", (req, res) => {
+  res.send("Hello World.");
+});
+
+deweyApi.listen(port, () => {
+  console.log(`Dewey API listening on port ${port}`);
+});
