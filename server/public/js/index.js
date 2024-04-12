0b010e48c60dd2d4f36dd8977156e67d7a768733
Eric Barch <eric.michael.barch@gmail.com>
2024-04-12 18:20:06 -0400

    Refactor frontend

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,162 +1,162 @@
 import { getHighlightedBookRow, setHighlightedBookRow } from "./global.js";
 
-const dbListener = () => {
-  const bookTableBody = document.getElementById("bookTableBody");
-  const bookRowTemplate = document.getElementById("bookRowTemplate");
+const getBook = async (isbn) => {
+  const response = await fetch(`/api/v1/book/${isbn}`, { method: "GET" });
+  const book = await response.json();
+  return book;
+};
 
-  const getAllBooks = async () => {
-    const response = await fetch("/api/v1/book");
-    const books = await response.json();
+const postBook = async (isbn) => {};
 
-    books.forEach((book) => {
-      const newBookRow = createNewBookRow(book);
-      bookTableBody.appendChild(newBookRow);
-    });
-  };
+const putBook = async (isbn) => {};
 
-  const createNewBookRow = (book) => {
-    const isbn = book.isbn13 || book.isbn10;
+const deleteBook = async (isbn) => {};
 
-    const newBookRow = document
-      .importNode(bookRowTemplate.content, true)
-      .querySelector("tr");
+const createBookRow = (book) => {
+  const bookRow = document
+    .importNode(bookRowTemplate.content, true)
+    .querySelector("tr");
 
-    newBookRow.setAttribute("data-isbn", isbn);
+  bookRow.setAttribute("data-isbn", book.scannedIsbn);
 
-    const cells = newBookRow.querySelectorAll("td");
+  const cells = bookRow.querySelectorAll("td");
 
-    cells.forEach((cell) => {
-      const className = cell.className;
-      const value = book[className];
+  cells.forEach((cell) => {
+    const className = cell.className;
+    const value = book[className];
 
-      if (className === "thumbnail") {
+    switch (className) {
+      case "thumbnail":
         const img = document.createElement("img");
         img.src = value;
         img.alt = `Book cover for ${book.title}`;
         cell.textContent = "";
         cell.appendChild(img);
-      } else if (className === "title") {
+        break;
+      case "title":
         const a = document.createElement("a");
         a.href = "javascript:void(0)";
         a.textContent = book.title;
         cell.appendChild(a);
-      } else if (className === "lcClassification") {
+        break;
+      case "lcClassification":
         const lcClassification = `${book.lcClass || ""}${book.lcTopic || ""} ${book.lcSubjectCutter || ""} ${book.lcAuthorCutter || ""}`;
         cell.textContent = lcClassification;
-      } else {
+        break;
+      default:
         cell.textContent = value;
-      }
-    });
-
-    return newBookRow;
-  };
-
-  const addBook = async (book) => {
-    const bookRows = bookTableBody.querySelectorAll("tr[data-isbn]");
-    const newBookRow = createNewBookRow(book);
-
-    for (const bookRow of bookRows) {
-      const dewey = bookRow.querySelector(".dewey")?.textContent;
-
-      if (book.dewey < dewey) {
-        bookRow.before(newBookRow);
-        return;
-      }
     }
+  });
 
-    bookTableBody.appendChild(newBookRow);
-  };
-
-  const updateBook = async (book) => {
-    const isbn = book.isbn13 || book.isbn10;
-
-    const bookRow = bookTableBody.querySelector(`tr[data-isbn="${isbn}"]`);
-
-    if (!bookRow) {
-      console.error(`Did not find book with ISBN ${isbn}.`);
-      throw new Error(`Did not find book with ISBN ${isbn}.`);
-    }
-
-    if (
-      bookRow.querySelector(".deweyClassification").textContent !==
-      book.deweyClassification
-    ) {
-      bookRow.remove();
-      await addBook(book);
-    } else {
-      /**Dewey number hasn't changed. No need to re-sort. */
-      const newBookRow = createNewBookRow(book);
-      bookRow.replaceWith(newBookRow);
-    }
-  };
-
-  const deleteBook = async (isbn) => {
-    const bookRow = bookTableBody.querySelector(`tr[data-isbn="${isbn}"]`);
-    bookRow.remove();
-  };
-
-  const eventSource = new EventSource("/api/v1/book-events");
-
-  eventSource.onmessage = async (event) => {
-    const { isbn } = JSON.parse(event.data);
-    const response = await fetch(`/api/v1/book/${isbn}`);
-    const { book, source } = await response.json();
-
-    if (source === "db") {
-      /**If the book came from the database, it should either be updated in or
-       * added to our displayed library. */
-      try {
-        await updateBook(book);
-      } catch {
-        await addBook(book);
-      }
-    } else {
-      /**If the book did not come from the database, it came from the fallback
-       * external API lookups. It should not be included in our displayed library. */
-      await deleteBook(isbn);
-    }
-  };
-
-  getAllBooks();
+  return bookRow;
 };
 
-const formListener = () => {
-  const searchBookForm = document.getElementById("searchBookForm");
-  const searchBookInput = document.getElementById("searchBookInput");
+const createAllBookRows = async () => {
+  const bookTableBody = document.getElementById("bookTableBody");
 
-  searchBookForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  const response = await fetch("/api/v1/book", { method: "GET" });
+  const books = await response.json();
 
-    const isbn = searchBookInput.value;
-    console.log("isbn", isbn);
-
-    const bookRow = document.querySelector(`tr[data-isbn="${isbn}"]`);
-
-    if (bookRow) {
-      console.log("Book exists.");
-
-      const highlightedBookRow = getHighlightedBookRow();
-
-      if (highlightedBookRow) {
-        highlightedBookRow.style.backgroundColor = "";
-      }
-
-      bookRow.style.backgroundColor = "lightyellow";
-      bookRow.scrollIntoView({ behavior: "smooth", block: "center" });
-
-      setHighlightedBookRow(bookRow);
-    } else {
-      console.log("Adding book.");
-      await fetch(`/api/v1/book/${isbn}`, {
-        method: "PUT",
-      });
-    }
-
-    searchBookInput.value = "";
+  books.forEach((book) => {
+    const bookRow = createBookRow(book);
+    bookTableBody.appendChild(bookRow);
   });
 };
 
-const rowClickListener = () => {
+const insertBookRow = async (isbn) => {
+  const book = getBook(isbn);
+  const newBookRow = createBookRow(book);
+
+  const bookRows = bookTableBody.querySelectorAll("tr[data-isbn]");
+
+  for (const bookRow of bookRows) {
+    const lcClassification =
+      bookRow.querySelector(".lcClassification")?.textContent;
+
+    if (book.lcClassification < lcClassification) {
+      bookRow.before(newBookRow);
+      return;
+    }
+  }
+
+  bookTableBody.appendChild(newBookRow);
+};
+
+const updateBookRow = async (isbn) => {
+  const book = getBook(isbn);
+  const bookRow = bookTableBody.querySelector(
+    `tr[data-isbn="${book.scannedIsbn}"]`,
+  );
+
+  if (!bookRow) {
+    console.error(`Did not find book with ISBN ${isbn}.`);
+    throw new Error(`Did not find book with ISBN ${isbn}.`);
+  }
+
+  const lcClassification =
+    bookRow.querySelector(".lcClassification")?.textContent;
+
+  if (lcClassification !== book.lcClassification) {
+    bookRow.remove();
+    await insertBookRow(isbn);
+  } else {
+    /**Classification hasn't changed. No need to re-sort. */
+    const newBookRow = createBookRow(book);
+    bookRow.replaceWith(newBookRow);
+  }
+};
+
+const deleteBookRow = async (isbn) => {
+  const bookRow = bookTableBody.querySelector(`tr[data-isbn="${isbn}"]`);
+  bookRow.remove();
+};
+
+const searchBook = async (event) => {
+  event.preventDefault();
+  const searchInput = document.getElementById("searchInput");
+
+  const isbn = searchInput.value;
+  const bookRow = document.querySelector(`tr[data-isbn="${isbn}"]`);
+
+  if (bookRow) {
+    const highlightedBookRow = getHighlightedBookRow();
+    highlightedBookRow && (highlightedBookRow.style.backgroundColor = "");
+
+    bookRow.style.backgroundColor = "lightyellow";
+    bookRow.scrollIntoView({ behavior: "smooth", block: "center" });
+
+    setHighlightedBookRow(bookRow);
+  } else {
+    const book = await getBook(isbn);
+    showPopup(book);
+  }
+
+  searchInput.value = "";
+};
+
+const bookEventListener = () => {
+  const eventSource = new EventSource("/api/v1/book-events");
+
+  eventSource.onmessage = async (event) => {
+    const { isbn, action } = JSON.parse(event.data);
+
+    switch (action) {
+      case "insertBookRow":
+        await insertBookRow(isbn);
+        break;
+      case "updateBookRow":
+        await updateBookRow(isbn);
+        break;
+      case "deleteBookRow":
+        await deleteBook(isbn);
+        break;
+      default:
+        throw new Error(`Unrecognized action: ${action}`);
+    }
+  };
+};
+
+const titleClickListener = () => {
   const bookTableBody = document.getElementById("bookTableBody");
 
   bookTableBody.addEventListener("click", function (event) {
@@ -201,6 +201,7 @@ const rowClickListener = () => {
   };
 };
 
-document.addEventListener("DOMContentLoaded", rowClickListener);
-document.addEventListener("DOMContentLoaded", dbListener);
-document.addEventListener("DOMContentLoaded", formListener);
+createAllBookRows();
+document.getElementById("searchForm").addEventListener("submit", searchBook);
+document.addEventListener("DOMContentLoaded", bookEventListener);
+document.addEventListener("DOMContentLoaded", titleClickListener);
