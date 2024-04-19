a056543fd19c744ab546bb3ce6c02a388207cc2e
Eric Barch <eric.michael.barch@gmail.com>
2024-04-19 17:36:38 -0400

    feat(web): Add postBook method to index.js

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -8,6 +8,14 @@ const getAllBooks = async () => {
   return books;
 };
 
+const postBook = async (book) => {
+  const isbn = book.details.isbn13 || book.details.isbn10;
+
+  await fetch(`/api/book/${isbn}`, {
+    method: "POST",
+  });
+};
+
 const getBook = async (isbn) => {
   const response = await fetch(`/api/book/${isbn}`, {
     method: "GET",
